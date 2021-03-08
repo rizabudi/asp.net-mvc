55b473e58eb827226c970c4a1bc5ccdb7557367b
Benediktus Anindito <bennybroz105@gmail.com>
2021-03-08 04:52:36 +0000

    fix build script in accordance with infrastructure configuration changes

--- build.bash
+++ build.bash
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1001/bus
-systemctl --user stop adminlte
+#export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1001/bus
+systemctl  stop adminlte
 rm -rf AdminLte/{obj,bin}
+#unlink dist/wwwroot/uploads
 rm -rf dist
 pushd AdminLte
 dotnet publish --configuration Release
@@ -10,6 +11,6 @@ popd
 cp -r AdminLte/bin/Release/netcoreapp3.1/publish dist
 ## TODO dirty hack maybe?
 cp -r AdminLte/Views dist/
-systemctl --user start adminlte
 rm -rf dist/wwwroot/uploads
 ln -s /srv/uploads dist/wwwroot/uploads
+systemctl  start adminlte
